b0a00af635dffa10a53e518ad694bf42e04e10a2
amatsukixprog <amatsukixprog@gmail.com>
2020-02-22 13:59:37 +0900

    AOJ ITP1_A

---
+++ matome.cpp
@@ -0,0 +1,17 @@
+// --- helloworld ---
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;
+    return 0;
+}
+
+// ---数値の入力---
+int x;
+cin >> x; //標準入力
+
+// ---１行で複数の数値の入力---
+int a;
+int b;
+cin >> a >> b;　// 変数a, b にこの順番で整数を入力する
